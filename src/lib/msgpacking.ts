eba3eb8babb70c0254995e58bcfc6f0b45824dff
person4268 <28717044+person4268@users.noreply.github.com>
2023-11-21 22:47:12 -0500

    more prototyping work

---
+++ src/lib/msgpacking.ts
@@ -0,0 +1,46 @@
+const QR_CODE_MAX_DATA = 1538; // version 32-Q
+
+import { encode } from "@msgpack/msgpack";
+import crc32 from "$lib/crc32"
+
+function hex(arrayBuffer: Uint8Array)
+{
+    return Array.prototype.map.call(
+        new Uint8Array(arrayBuffer),
+        n => n.toString(16).padStart(2, "0")
+    ).join("");
+}
+
+export interface Data {
+  lowScore: number;
+  highScore: number;
+  midScore: number;
+  stTest: string;
+}
+
+export function encodeAndSegment(data: Data) {
+  debugger;
+  const encoded = encode(data);
+
+  // first byte is amount of codes, second byte is current code number,then segment things up if encoded length > 1538-2, then a crc32 at the end
+  const segmentSize = QR_CODE_MAX_DATA - 2 - 4;
+  console.log(encoded.length, segmentSize);
+  const segments = Math.ceil(encoded.length / segmentSize);
+
+  let out = [];
+  for (let i = 0; i < segments; i++) {
+    const segment = encoded.slice(i * segmentSize, (i + 1) * segmentSize);
+    console.log(i, segment.length, i * segmentSize, (i + 1) * segmentSize);
+    const segmentBuffer = new Uint8Array(segmentSize + 2 + 4);
+    segmentBuffer[0] = segments;
+    segmentBuffer[1] = i;
+    segmentBuffer.set(segment, 2);
+    const crc = crc32(segmentBuffer.slice(0, segmentBuffer.length - 4));
+    console.log(i, crc, hex(crc))
+    segmentBuffer.set(crc, segmentBuffer.length - 4);
+    console.log(hex(segmentBuffer))
+    out.push(segmentBuffer);
+  }
+
+  return out;
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
   import QRCode from 'qrcode'
+  import { encodeAndSegment } from '$lib/msgpacking'
 
   let highScore = 0;
   let lowScore = 0;
   let midScore = 0;
 
   let str = "";
+  let stTest = "this can be a lot of text but i didn't wanna commit 800 words of lorem ipsum";
 
   let canvas: HTMLCanvasElement;
 
   async function submit() {
     str = `${highScore} ${lowScore} ${midScore}`
-    QRCode.toCanvas(canvas, str, {errorCorrectionLevel: 'Q', version: 25}, function (error) {
+    let test = encodeAndSegment({highScore, lowScore, midScore, stTest});
+    console.log(test);
+    QRCode.toCanvas(canvas, str, {errorCorrectionLevel: 'M', version: 32}, function (error) {
       if (error) console.error(error)
       console.log('success!');
     })
@@ -21,9 +25,9 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<p>High node score: <input type="number" min=0, max=100 bind:value={highScore}/></p>
-<p>Low node score: <input type="number" min=0, max=100 bind:value={lowScore}/></p>
-<p>Mid score: <input type="number" min=0, max=100 bind:value={midScore}/></p>
+<p>High node score: <input type="number" min=0 max=100 bind:value={highScore}/></p>
+<p>Low node score: <input type="number" min=0 max=100 bind:value={lowScore}/></p>
+<p>Mid score: <input type="number" min=0 max=100 bind:value={midScore}/></p>
 
 <button on:click={submit}>Submit</button>
 
