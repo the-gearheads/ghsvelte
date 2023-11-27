1605c0527775ddbebc6838cb5da5f21ba372166a
Gavin Perry <Gavinoperry9@gmail.com>
2023-11-26 20:34:38 -0500

    Added short input component

---
+++ src/form elements/InputShort.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Input } from 'stwui';
+
+    export let label = "";
+    export let value = "";
+
+</script>
+
+<Input bind:value={value} name="input">
+	<Input.Label slot="label">{label}</Input.Label>
+</Input>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import Checkbox from "../form elements/Checkbox.svelte";
+	import InputShort from "../form elements/InputShort.svelte";
 
     let selected: Record<string, boolean> = {};
-
+    let value: string;
     let options = [
         {
             name: 'unlimited games, but no games',
@@ -22,13 +23,15 @@
         {
             name: 'hoot hoot',
             value: 'hoot hoot',
-            label: 'hoot hoot',
+            label: 'hoot hosefesfot',
         }
     ]
     let question = "Would you rather have unlimited bacon, but no more games. Or games, unlimited games, but no games"
 </script>
-<Checkbox {question} {options} bind:selected={selected}></Checkbox>
+<Checkbox {question} {options} bind:selected></Checkbox>
+<InputShort label="What is the capital of ecuador?" bind:value></InputShort>
 
+<b>You entered: {value}</b>
 <br><br><h4>Here's what you selected:</h4>
 {#each Object.keys(selected) as key}
     <p><b>{key}:</b> {selected[key]}</p>
