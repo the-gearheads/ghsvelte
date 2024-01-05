d683224f0e6dd7ac1aeead51172e88f3007ceae3
person4268 <28717044+person4268@users.noreply.github.com>
2024-01-05 12:19:32 -0500

    actually remove menu

---
+++ src/lib/store.ts
@@ -0,0 +1,32 @@
+import { browser } from '$app/environment';
+import type { Writable } from 'svelte/store';
+import { writable, get } from 'svelte/store'
+
+/* localStorage backed store, from https://stackoverflow.com/a/68785061 */
+const storage = <T>(key: string, initValue: T): Writable<T> => {
+  const store = writable(initValue);
+  if (!browser) return store;
+
+  const storedValueStr = localStorage.getItem(key);
+  if (storedValueStr != null) store.set(JSON.parse(storedValueStr));
+
+  store.subscribe((val) => {
+    if (val != null || val != undefined) {
+      localStorage.removeItem(key)
+    } else {
+      localStorage.setItem(key, JSON.stringify(val))
+    }
+  })
+
+  window.addEventListener('storage', () => {
+    const storedValueStr = localStorage.getItem(key);
+    if (storedValueStr == null) return;
+
+    const localValue: T = JSON.parse(storedValueStr)
+    if (localValue !== get(store)) store.set(localValue);
+  });
+
+  return store;
+}
+
+export default storage
