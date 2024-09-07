e8b07c6c5b63078e60153f515da8540f9228d4cb
person4268 <28717044+person4268@users.noreply.github.com>
2024-09-07 14:36:31 -0400

    warmup some things to improve dev server speed

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,19 @@ export default defineConfig({
 	css: {
 		devSourcemap: true,
 	},
+	server: {
+    warmup: {
+      clientFiles: [
+				'/.svelte-kit/generated/root.svelte',
+				'/node_modules/@sveltejs/kit/src/runtime/server/utils.js',
+				'/node_modules/@sveltejs/kit/src/runtime/server/page/actions.js',
+				'/node_modules/@sveltejs/kit/src/runtime/server/cookie.js',
+				'/node_modules/@sveltejs/kit/src/runtime/server/page/render.js',
+        '/src/app.pcss',
+				'/src/app.pcss?inline=',
+        '/src/routes/(frontend)/+layout.svelte?svelte&type=style&lang.css',
+				'/src/routes/(frontend)/+layout.svelte'
+      ],
+    },
+  },
 });
